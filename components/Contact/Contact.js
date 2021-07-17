90b147adfa4319c774cf2cefc8a69ece003ab5a7
ishanjirety <ishanjirety24@gmail.com>
2021-07-18 03:03:24 +0530

    feat: added mailing service

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -2,15 +2,18 @@ import React, { useEffect, useState } from 'react'
 import { useRoute } from '../../Context'
 import Arrow from '../../Images/Arrow.svg'
 import Image from 'next/image'
-import { Card } from '../Card/Card'
+import emailjs from 'emailjs-com';
+
 export const Contact = () => {
-    const { route } = useRoute()
+    const { route,setRoute } = useRoute()
     const [slide, setSlide] = useState('')
     const [isHidden, setIsHidden] = useState('hidden')
     const [email, setEmail] = useState('')
     const [message, setMessage] = useState('')
     const [name, setName] = useState('')
-    const { setRoute } = useRoute()
+    const [loading,setLoading] = useState('Send')
+    const [sent, setSent] = useState(undefined)
+
 
     function changePage() {
         setTimeout(() => {
@@ -27,10 +30,20 @@ export const Contact = () => {
         }
     }, [route])
 
-    function onSubmitHandler(e) {
-        e.preventDefault();
-        console.log(e)
-        console.log('clicked')
+    async function onSubmitHandler(e) {
+        try {
+            e.preventDefault();
+            setLoading('Sending...')
+            await emailjs.sendForm(process.env.SERVICE_ID, process.env.TEMPLATE_ID, e.target, process.env.USER_ID)
+            setEmail("")
+            setMessage("")
+            setName("")
+            setSent(true)
+            setLoading('Send')
+        } catch (e) {
+            setSent(false)
+            setLoading('Send')
+        }
     }
     function updateText(action, value) {
         switch (action) {
@@ -48,7 +61,7 @@ export const Contact = () => {
 
 
     return (
-        <div className={`${slide} md:z-10 h-full pl-6 pr-3 pt-20 md:pt-16 justify-center text-white-50 font-poppins ${isHidden}`}>
+        <div className={`${slide} md:z-10 h-full pl-6 pr-3 pt-20 md:pt-8 justify-center text-white-50 font-poppins ${isHidden}`}>
             <h1 className="text-3xl md:pl-56 md:text-8xl"><span className="text-aqua-600">L</span>et&apos;s <span className="text-aqua-600">C</span>onnect</h1>
             <p className="text-sm pt-2 md:pl-56 md:w-4/5 md:pt-4">I&apos;d love to hear about what you&apos;re working on and how I could help. I&apos;m currently looking for a new role and open to opportunities in the domain of web development. I&apos;m also open to discussing ideas and doing a project together.
             </p>
@@ -56,14 +69,17 @@ export const Contact = () => {
             </p>
 
             <form className=" mt-3 flex flex-col gap-y-3" onSubmit={onSubmitHandler}>
-                
 
-                <input value={name} className=" z-10 md:ml-56 md:pl-4 p-2 w-72 bg-gray-950 rounded btn-emboss-plus outline-none" placeholder="Name" onChange={(e) => updateText("NAME", e.target.value)}></input>
 
-                <input value={email} className=" z-10 md:ml-56 md:pl-4 p-2 w-72 bg-gray-950 rounded btn-emboss-plus outline-none" placeholder="Email" onChange={(e) => updateText("EMAIL", e.target.value)}></input>
+                <input value={name} name="name" className=" z-10 md:ml-56 md:pl-4 p-2 w-72 bg-gray-950 rounded btn-emboss-plus outline-none" placeholder="Name" onChange={(e) => updateText("NAME", e.target.value)}></input>
 
-                <textarea value={message} className=" z-10 md:w-1/2 md:pl-4 p-2 md:ml-56 rounded btn-emboss-plus h-20 md:h-44 bg-gray-950 resize-none outline-none" placeholder="Message" onChange={(e) => updateText("MESSAGE", e.target.value)}></textarea>
-                <button className="bg-gray-950 z-10 md:ml-56  text-aqua-600 btn-emboss-plus w-36 p-2 ">Send</button>
+                <input value={email} name="email" className=" z-10 md:ml-56 md:pl-4 p-2 w-72 bg-gray-950 rounded btn-emboss-plus outline-none" placeholder="Email" onChange={(e) => updateText("EMAIL", e.target.value)}></input>
+
+                <textarea name="message" value={message} className=" z-10 md:w-1/2 md:pl-4 p-2 md:ml-56 rounded btn-emboss-plus h-20 md:h-44 bg-gray-950 resize-none outline-none" placeholder="Message" onChange={(e) => updateText("MESSAGE", e.target.value)}></textarea>
+
+                <p className={`text-green-600 md:pl-56 p-0 ${sent===true ? 'visible' : 'hidden'} `}>Thank you for your message!! I will get back to you soon :)</p>
+                <p className={`text-red-600 md:pl-56 p-0 ${sent===false ? '' : 'hidden'} `}>Oops! something went wrong. you can directly mail to <strong>ishanjirety24@gmail.com</strong></p>
+                <button className="bg-gray-950 z-10 md:ml-56  text-aqua-600 btn-emboss-plus w-36 p-2 ">{loading}</button>
             </form>
 
             <button type="submit" onClick={changePage} className={`z-10 bounce btn-emboss absolute bottom-4 right-0 left-0 m-auto bg-gray-950 rounded-full p-3 flex items-center justify-center `}><Image src={Arrow} className="rotate" /></button>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+require('dotenv').config()
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com','images.pexels.com','*','ishan-jirety.hashnode.dev'],
-},
+    domains: ['images.unsplash.com', 'images.pexels.com', '*', 'ishan-jirety.hashnode.dev'],
+  },
+  env: {
+    SERVICE_ID: process.env.SERVICE_ID,
+    TEMPLATE_ID: process.env.TEMPLATE_ID,
+    USER_ID: process.env.USER_ID,
+  }
 }
