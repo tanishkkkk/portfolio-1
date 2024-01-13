4b9813acbf0f34eb093b95eb3cefb0a0506efa89
tanishkkkk <tanishk@browserstack.com>
2024-01-13 14:46:50 +0530

    fix: home

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,26 +1,45 @@
-import React, { useState } from 'react'
-import Arrow from '../../Images/Arrow.svg'
-import Image from 'next/image'
-import { useRoute } from '../../Context'
+import React, { useState } from "react";
+import Arrow from "../../Images/Arrow.svg";
+import Image from "next/image";
+import { useRoute } from "../../Context";
 export const Home = () => {
-    const [slide, setSlide] = useState('')
-    const { setRoute } = useRoute()
-    function changePage() {
-        setTimeout(() => {
-            setRoute('about')
-        }, 150)
-        setSlide('slideUp')
-    }
-    return (
-        <>
-            <div className={`${slide} index-high h-full pl-10 flex items-start  flex-col justify-center text-white-50 font-poppins font-black text-5xl md:pl-60 md:text-8xl`}>
-                <h1 className="index-high tracking-widest pt-2"><span className="text-aqua-600" ></span>Hi</h1>
-                <h1 className="index-high tracking-widest pt-2">I&apos;m <span className="text-aqua-600">Ishan</span></h1>
-                <h1 className="index-high tracking-widest text-3xl md:text-7xl pt-2">web <span className="text-aqua-600">d</span>eveloper</h1>
-                <h3 className="index-high font-medium text-sm md:text-xl  text-gray-850 pt-2">JavaScript <span className="text-aqua-450">/</span> MERN <span className="text-aqua-450">/</span> NextJs <span className="text-aqua-450">/</span> GraphQl <span className="text-aqua-450">/</span> Hasura <span className="text-aqua-450">/</span> Firebase <span className="text-aqua-450">/</span> Tailwind </h3>
-                <button onClick={changePage} className={`bounce w-16 h-16 btn-emboss absolute bottom-4 right-0 left-0 m-auto bg-gray-950 rounded-full p-3 flex items-center justify-center`}><Image src={Arrow} /></button>
-            </div>
-
-        </>
-    )
-}
+  const [slide, setSlide] = useState("");
+  const { setRoute } = useRoute();
+  function changePage() {
+    setTimeout(() => {
+      setRoute("about");
+    }, 150);
+    setSlide("slideUp");
+  }
+  return (
+    <>
+      <div
+        className={`${slide} index-high h-full pl-10 flex items-start  flex-col justify-center text-white-50 font-poppins font-black text-5xl md:pl-60 md:text-8xl`}
+      >
+        <h1 className="index-high tracking-widest pt-2">
+          <span className="text-aqua-600"></span>Hi
+        </h1>
+        <h1 className="index-high tracking-widest pt-2">
+          I&apos;m <span className="text-aqua-600">Tanishk</span>
+        </h1>
+        <h1 className="index-high tracking-widest text-3xl md:text-7xl pt-2">
+          Web <span className="text-aqua-600 ml-2">D</span>eveloper
+        </h1>
+        <h3 className="index-high font-medium text-sm md:text-xl  text-gray-850 pt-2">
+          JavaScript <span className="text-aqua-450">/</span> MERN{" "}
+          <span className="text-aqua-450">/</span> NextJs{" "}
+          <span className="text-aqua-450">/</span> GraphQl{" "}
+          <span className="text-aqua-450">/</span> Typescript{" "}
+          <span className="text-aqua-450">/</span> Firebase{" "}
+          <span className="text-aqua-450">/</span> Tailwind{" "}
+        </h3>
+        <button
+          onClick={changePage}
+          className={`bounce w-16 h-16 btn-emboss absolute bottom-4 right-0 left-0 m-auto bg-gray-950 rounded-full p-3 flex items-center justify-center`}
+        >
+          <Image src={Arrow} alt="down-arrow" />
+        </button>
+      </div>
+    </>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,39 @@
-import React from 'react'
-import { Layout, Home, About, Projects, Blogs, Contact, Skills } from '../components'
-import { useRoute } from '../Context'
-import Head from 'next/head'
+import React from "react";
+import {
+  Layout,
+  Home,
+  About,
+  Projects,
+  Blogs,
+  Contact,
+  Skills,
+} from "../components";
+import { useRoute } from "../Context";
+import Head from "next/head";
 
 const Portfolio = () => {
-    const { route } = useRoute()
-    return (
-        <Layout >
-            <Head>
-                <title>Ishan Jirety</title>
-                <meta name="description" content="Hi, I am a Frontend Developer. I have been working on the MERN stack for about 6 months in this span I have developed multiple projects which helped me to enhance my skills." />
-                <meta property="og:title" content="Ishan Jirety portfolio website" />
-                <meta property="og:url" content="https://ishanjirety.vercel.app" />
-                <meta property="og:type" content="website" />
-            </Head>
-            <div className="bg-100 w-full h-screen overflow-hidden p-0 m-0">
-
-                {route === 'home' && <Home />}
-                {route === 'about' && <About />}
-                {route === 'skills' && <Skills />}
-                {route === 'projects' && <Projects />}
-                {route === 'blogs' && <Blogs />}
-                {route === 'contact' && <Contact />}
-            </div>
-        </Layout>
-    )
-}
-export default Portfolio
+  const { route } = useRoute();
+  return (
+    <Layout>
+      <Head>
+        <title>Tanishk Sharma</title>
+        <meta
+          name="description"
+          content="Hi, I am a Frontend Developer. I have been working on the MERN stack for about 6 months in this span I have developed multiple projects which helped me to enhance my skills."
+        />
+        <meta property="og:title" content="Ishan Jirety portfolio website" />
+        <meta property="og:url" content="https://ishanjirety.vercel.app" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <div className="bg-100 w-full h-screen overflow-hidden p-0 m-0">
+        {route === "home" && <Home />}
+        {route === "about" && <About />}
+        {route === "skills" && <Skills />}
+        {route === "projects" && <Projects />}
+        {route === "blogs" && <Blogs />}
+        {route === "contact" && <Contact />}
+      </div>
+    </Layout>
+  );
+};
+export default Portfolio;
