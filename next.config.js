580d63625e7a2651f29425cbc759062611887eea
tanishkkkk <tanishk@browserstack.com>
2024-02-05 00:29:54 +0530

    feat: added bogs

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
-require('dotenv').config()
+require("dotenv").config();
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'images.pexels.com', '*', 'ishan-jirety.hashnode.dev'],
+    domains: [
+      "images.unsplash.com",
+      "images.pexels.com",
+      "*",
+      "media.istockphoto.com",
+      "media.dev.to",
+    ],
   },
   env: {
     SERVICE_ID: process.env.SERVICE_ID,
     TEMPLATE_ID: process.env.TEMPLATE_ID,
     USER_ID: process.env.USER_ID,
-  }
-}
+  },
+};
